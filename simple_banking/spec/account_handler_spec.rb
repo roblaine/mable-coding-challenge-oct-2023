7a69a928563335805146068dce8b4fee4952396b
Rob Laine <rlainewyllie@gmail.com>
2023-10-06 12:39:56 +1100

    Write tests for AccountHandler

--- simple_banking/spec/account_handler_spec.rb
+++ simple_banking/spec/account_handler_spec.rb
@@ -4,8 +4,9 @@ require "simple_banking/account_handler"
 require "simple_banking/errors"
 
 RSpec.describe AccountHandler do
-  @default_opening_balance = 0.0
-  @default_acc_id = "1234123412341234"
+  @default_opening_balance  = 0.0
+  @default_acc_id           = "1234123412341234"
+  @recipient                = "1234123412341230"
 
   before(:each) do
     @ac = AccountHandler.new
@@ -42,47 +43,42 @@ RSpec.describe AccountHandler do
 
   describe "read/1" do
     it "does read the account balance of a valid account" do
-      @ac.store(@default_acc_id, @default_opening_balance)
+      accounts = AccountHandler.new({@default_acc_id => @default_opening_balance})
       
-      expect(@ac.read(acc_id)).to eql(@default_opening_balance)
+      expect(accounts.read(acc_id)).to eql(@default_opening_balance)
     end
 
     it "does not read the account balance of a non-existant account" do
-      expect(@ac.read(@default_acc_id)).to eql(nil)
+      non_acc_id = "9999999999999999"
+      expect(@ac.read(non_acc_id)).to eql(nil)
     end
   end
 
   describe "withdraw/2" do
     it "does withdraw an amount from an account" do
-      opening_balance = 100.0
-      withdraw = 30.0
-
-      @ac.store(@default_acc_id, opening_balance)
-      @ac.withdraw(@default_acc_id, withdraw)
-      new_balance = @ac.read(@default_acc_id)
+      opening_balance, withdraw = 100.0, 30.0
+      accounts = AccountHandler.new({@default_acc_id => opening_balance})
+      accounts.withdraw(@default_acc_id, withdraw)
       
-      expect(new_balance).to eql(opening_balance - withdraw)
+      expect(accounts.read(@default_acc_id)).to eql(opening_balance - withdraw)
     end
     
     it "does not put an account into overdraft" do
-      opening_balance = 30.0
-      withdraw = 100.0
-
-      @ac.store(@default_acc_id, opening_balance)
+      opening_balance, withdraw = 30.0, 100.0
+      accounts = AccountHandler.new({@default_acc_id => opening_balance})
       
-      expect{ @ac.withdraw(@default_acc_id, withdraw) }.to raise_error(Errors::OverDraftError)
-      expect(@ac.read(@default_acc_id)).to eql(opening_balance)
+      expect{ accounts.withdraw(@default_acc_id, withdraw) }.to raise_error(Errors::OverDraftError)
+      expect(accounts.read(@default_acc_id)).to eql(opening_balance)
     end
   end
   
   describe "deposit/2" do
     it "does deposit a value into a valid account id" do
-      opening_balance = 100.0
-
-      @ac.store(@default_acc_id, opening_balance)
-      @ac.deposit(@default_acc_id, deposit)
+      opening_balance, deposit = 100.0, 10.0
+      accounts = AccountHandler.new({@default_acc_id => opening_balance})
+      accounts.deposit(@default_acc_id, deposit)
       
-      expect(@ac.read(@default_acc_id)).to eql(opening_balance + deposit)    
+      expect(accounts.read(@default_acc_id)).to eql(opening_balance + deposit)    
     end
 
     it "does not deposit to an account that does not exist" do
@@ -92,21 +88,41 @@ RSpec.describe AccountHandler do
 
   describe "transfer/3" do
     it "does transfer funds from one account to another" do
-      
+      opening_balance, recipient_funds = 100.0, 0.0
+      accounts = AccountHandler.new({@default_acc_id => opening_balance, @recipient => recipient_funds})
+
+      accounts.transfer(@default_acc_id, @recipient, opening_balance)
+
+      expect(accounts.read(@default_acc_id)).to eql(recipient_funds)
+      expect(accounts.read(@recipient)).to eql(opening_balance)
     end
     
     it "does not transfer funds if it would leave the account in overdraft" do
-      
+      opening_balance, recipient_funds = 0.0, 100.0
+      accounts = AccountHandler.new({@default_acc_id => opening_balance, @recipient => recipient_funds})
+
+      expect { accounts.transfer(@default_acc_id, @recipient, opening_balance) }.to raise_error(Errors::OverDraftError)
+
+      expect(accounts.read(@default_acc_id)).to eql(opening_balance)
+      expect(accounts.read(@recipient)).to eql(recipient_funds)
     end
   end
 
   describe "has_balance/2" do
-    it "does return true if the account has required funds" do
+    it "does return true if the account has the required funds" do
+      opening_balance = 100.0
+      accounts = AccountHandler.new({@default_acc_id => opening_balance})
 
+      expect(accounts.has_balance?(opening_balance)).to eql(true)
+      expect(accounts.has_balance?(99.99)).to eql(true)
     end
 
     it "does not return true if the account hasn't required funds" do
+      opening_balance = 0.0
+      accounts = AccountHandler.new({@default_acc_id => opening_balance})
 
+      expect(accounts.has_balance?(1.0)).to eql(false)
+      expect(accounts.has_balance?(0.01)).to eql(false)
     end
   end
 end
