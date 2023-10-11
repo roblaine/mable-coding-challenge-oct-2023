7432a5ca143261bcbe89945d99ba756856a9c3a1
Rob Laine <rlainewyllie@gmail.com>
2023-10-11 13:19:17 +1100

    Split functionality of setup accounts and read file

--- simple_banking/lib/simple_banking/simple_bank.rb
+++ simple_banking/lib/simple_banking/simple_bank.rb
@@ -17,15 +17,23 @@ class SimpleBank
     @default_transfer_path = "../mable_trans.csv"
   end
 
-  def setup_accounts(path=@default_acc_path)
+  def setup_from_file(file_path=@default_acc_path)
     begin
-      account_inputs = @parser.read(path)
-      # open_acc/2 returns nil when successful, and the acc id when failing the attempt. We can collect these in a new list
-      failures = account_inputs.map { |acc| @accounts.open_acc(acc.first, acc.last) }
-      failures.compact
+      self.setup_accounts(@parser.read(file_path))
     rescue Errno::ENOENT
       Errno::ENOENT
+    rescue fte=Errors::FileTypeError
+      puts fte.new().exception(file_path)
+      file_path
     end
+  end
+
+  def setup_accounts(accounts=[])
+    # open_acc/2 returns nil when successful, and the acc id when failing the attempt. We can collect these in a new list
+    accounts.map { |acc_id, balance| 
+      puts "#{acc_id} #{balance}"
+      @accounts.open_acc(acc_id, balance) 
+    }.compact()
   end
   
   def execute_transfers(path)
@@ -33,4 +41,6 @@ class SimpleBank
 
   def report
   end
+
+  attr_reader :accounts
 end
