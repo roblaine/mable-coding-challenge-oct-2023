20b37b7bfb15cd2da3f4470f1e2f97c94c9ca1d7
Rob Laine <rlainewyllie@gmail.com>
2023-10-12 12:01:50 +1100

    Update specs to capture failure/error states from transfers

--- simple_banking/spec/simple_bank_spec.rb
+++ simple_banking/spec/simple_bank_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe SimpleBank do
     @bank = SimpleBank.new
 
     @sample_csv_path = "./spec/fixtures/sample_acc_balance.csv"
-    @transfers_csv_path = "./spec/fixtures/sample_transfers_balance.csv"
+    @transfers_csv_path = "./spec/fixtures/sample_transfers.csv"
     @non_csv_path = "./spec/fixtures/non_csv.txt"
   end
 
@@ -54,46 +54,63 @@ RSpec.describe SimpleBank do
       expect(@bank.accounts.accounts.keys.sort).to eql(expected_accounts.sort)
     end
 
-    it "does returns the filepath on a non csv file" do
+    it "does return the filepath on a non csv file" do
       expect(@bank.setup_from_file(@non_csv_path)).to eql(@non_csv_path)
     end
   end
 
   describe "execute_transfers_from_file/1" do
     it "does read a csv file and perform transfers" do
-      # IDs from transfer file
+      # IDs from @transfers_csv_path file
       account_id, opening_balance = "6666234522226789", 100.0
       recipient_id, recipient_balance = "1111834566661834", 0.0
 
       acc_handler = AccountHandler.new({account_id => opening_balance, recipient_id => recipient_balance})
       bank = SimpleBank.new(acc_handler)
 
-      failed_transfers = @bank.execute_transfers_from_file(@transfers_csv_path)
+      failed_transfers = bank.execute_transfers_from_file(@transfers_csv_path)
 
       expect(failed_transfers).to eql([])
-      expect(@bank.accounts.read(recipient_id)).to eql(recipient_balance + opening_balance)
+      expect(bank.accounts.accounts[recipient_id]).to eql(recipient_balance + opening_balance)
     end
 
-    it "does returns the filepath on a non csv file" do
+    it "does return the filepath on a non csv file" do
       expect(@bank.execute_transfers_from_file(@non_csv_path)).to eql(@non_csv_path)
     end
   end
 
   describe "execute_transfers/1" do
-    it "does perform the transfers" do
-      fail
+    before(:each) do
+      @account_id, @opening_balance = "6666234522226789", 100.0
+      @recipient_id, @recipient_balance = "1111834566661834", 0.0
+      @transfer_amount = 100.0
+
+      acc_handler = AccountHandler.new({@account_id => @opening_balance, @recipient_id => @recipient_balance})
+      @transfer_tests_bank = SimpleBank.new(acc_handler)
     end
 
-    it "does return a list of failed transfers" do
-      fail
+    it "does perform the transfers" do
+      p @transfer_tests_bank
+      failures = @transfer_tests_bank.execute_transfers([[@account_id, @recipient_id, @transfer_amount]])
+
+      expect(failures).to eql([])
+      expect(@transfer_tests_bank.accounts.read(@account_id)).to eql(@opening_balance - @transfer_amount)
+      expect(@transfer_tests_bank.accounts.read(@recipient_id)).to eql(@recipient_balance + @transfer_amount)
+    end
+
+    it "does not perform an overdraft & returns a list of failed transfers by from account_id" do
+      failures = @transfer_tests_bank.execute_transfers([[@account_id, @recipient_id, @transfer_amount + 1.0]])
+
+      expect(failures).to eql([@account_id])
+      expect(@transfer_tests_bank.accounts.read(@account_id)).to eql(@opening_balance)
+      expect(@transfer_tests_bank.accounts.read(@recipient_id)).to eql(@recipient_balance)
     end
 
     it "does not perform transfers to invalid account ids" do
-      fail
-    end
+      failures = @transfer_tests_bank.execute_transfers([[@account_id, "made_up_id", @transfer_amount]])
 
-    it "does not perform transfers if account lacks funds" do
-      fail
+      expect(failures).to eql([@account_id])
+      expect(@transfer_tests_bank.accounts.read(@account_id)).to eql(@opening_balance)
     end
   end
 
